380425894ce30a7b6d1d29af49f5205822f59ade
Quincy Spurlock <165756458+spurlockq@users.noreply.github.com>
2026-03-24 22:10:05 -0700

    Add files via upload

---
+++ oil_door.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int mapping = map(joystickRead, 0, 1023, 400, 600);
+}
